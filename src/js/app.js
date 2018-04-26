2419a22613111c37ce4760ee28ca2884dbacac95
Gerry915 <gao88915@gmail.com>
2018-04-26 18:41:54 +1000

    fin

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,71 @@
-    var mainImg = document.getElementsByClassName("main-img")
-    var blurImg = document.getElementsByClassName("blur-img")
+var mainImg = document.getElementById("main-img")
+var blurImg = document.getElementById("blur-img")
     //getElementsByClassName return an array using [] when call the var
 
 function myFun(){
     
-    var distancePX = $(document).scrollTop();
+    var distancePX = $(document).scrollTop()
+    var test = document.getElementById("test")
+    var subtitle = document.getElementById("sub-title")
+    var nav = document.getElementById("nav")
     if (distancePX <= 300){
-    console.log(distancePX)
-    console.log(mainImg)
+        mainImg.classList.add("show")
+        mainImg.classList.remove("hide")
+
+        subtitle.classList.add("hide")
+        subtitle.classList.remove("show")
+
+        test.classList.add("show")
+        test.classList.remove("hide")
+        nav.style.background = "transparent"
+        nav.classList.remove("navbar-light")
+        nav.classList.remove("bg-light")
+
     }else{
-        console.log("Not there yet")
-        console.log(blurImg)
+        mainImg.classList.add("hide")
+        mainImg.classList.remove("show")
+
+        subtitle.classList.add("show")
+        subtitle.classList.remove("hide")
+
+        test.classList.add("hide")
+        test.classList.remove("show")
+
+        nav.classList.add("navbar-light")
+        nav.classList.add("bg-light")
+    }
+
+
+}
+
+
+
+
+function showSection(buttion_id){
+    var warehouse = document.getElementById("warehouse-list")
+    var distribution = document.getElementById("distribution-list")
+    var fleet = document.getElementById("fleet-list")
+
+    if(buttion_id === "warehouse"){
+        console.log(warehouse)
+        warehouse.style.display = "block"
+        distribution.style.display = "none"
+        fleet.style.display = "none"
+
+
+    }else if(buttion_id ==="distribution"){
+        console.log(distribution)
+
+        warehouse.style.display = "none"
+        distribution.style.display = "block"
+        fleet.style.display = "none"
+
+    }else if(buttion_id ==="fleet"){
+        console.log(fleet)
+
+        warehouse.style.display = "none"
+        distribution.style.display = "none"
+        fleet.style.display = "block"
+
     }
 }
